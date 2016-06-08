82a99c621e8406f20f4308a2363fbbb7be53bbca
Ryan Kurte <ryan.kurte@trutest.co.nz>
2016-06-08 17:24:26 +1200

    Fix mount script to mount /boot properly

--- mount.sh
+++ mount.sh
@@ -34,7 +34,7 @@ P1_INFO=($`fdisk -l $LOOP_BASE | grep ${LOOP_BASE}p1`)
 P2_INFO=($`fdisk -l $LOOP_BASE | grep ${LOOP_BASE}p2`)
 
 # Locate partition 2 start address
-P1_START=${P2_INFO[1]}
+P1_START=${P1_INFO[1]}
 P2_START=${P2_INFO[1]}
 
 echo "Located partitions: p1 at $P1_START p2 at $P2_START"
@@ -44,8 +44,8 @@ losetup -d $LOOP_BASE
 echo "Closed loopback $LOOP_BASE"
 
 mkdir -p $2
-mount $1 -o loop,offset=$(($P1_START*$BLOCK_SIZE)),rw $2
-mount $1 -o loop,offset=$(($P2_START*$BLOCK_SIZE)),rw $2/boot
+mount $1 -o loop,offset=$(($P2_START*$BLOCK_SIZE)),rw $2
+mount $1 -o loop,offset=$(($P1_START*$BLOCK_SIZE)),rw $2/boot
 
 echo "Mounted to $2 and $2/boot"
 
